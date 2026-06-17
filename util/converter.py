58edc6e3b400a8fe156e0f20de9e1b35859e7971
rcresponiuoc <rcresponi@uoc.edu>
2026-06-17 22:04:19 +0200

    se definen los converters

--- util/converter.py
+++ util/converter.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
-#Write your code here
+from products import *
+from users import *
 
 class Converter(ABC):
   @abstractmethod
@@ -10,14 +11,45 @@ class Converter(ABC):
       print(item.describe())
 
 class CashierConverter(Converter):
-  def convert(self,dataFrame):    
-    #Write your code here
-    pass
+  def convert(self,dataFrame):
+    cashiers = []
+    for _, row in dataFrame.iterrows():
+      cashier = Cashier(
+          dni=str(row['dni']),
+          name=row['name'],
+          age=row['age'],
+          timeTable=row['timetable'],
+          salary=row['salary']
+      )
+      cashiers.append(cashier)
+    return cashiers
 
 class CustomerConverter(Converter):
-  #Write your code here
-  pass
-
+  def convert(self,dataFrame):    
+    customers = []
+    for _, row in dataFrame.iterrows():
+      customer = Customer(
+          dni=str(row['dni']),
+          name=row['name'],
+          age=row['age'],
+          email=row['email'],
+          postalCode=row['postalcode']
+      )
+      customers.append(customer)
+    return customers
+  
 class ProductConverter(Converter):
-  #Write your code here
-  pass
+  def convert(self,dataFrame , *args):    
+    products = []
+    product_class = args[0]
+    valid_classes = [Hamburger, Soda, Drink, HappyMeal]
+    if args[0] not in valid_classes:
+      raise ValueError(f"Tipo de producto no válido: {args[0]}")
+    for _, row in dataFrame.iterrows():
+      product = product_class(
+        id=str(row['id']),
+        name=row['name'],
+        price=row['price']
+        )
+      products.append(product)
+    return products
